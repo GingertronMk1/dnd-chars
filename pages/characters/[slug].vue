7af9b1dcf71eb9c95003f2d3c3a07e88749e4224
Jack Ellis <jackellis1504@gmail.com>
2023-03-06 14:20:54 +0000

    Make it a bit more straightforward

--- pages/characters/[slug].vue
+++ pages/characters/[slug].vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-const { params } = useRoute()
-// When accessing /posts/1, route.params.id will be 1
-const { data } = await useAsyncData(`${params.route}`, () => {
-  return queryContent().findOne();
-}) as any
+const { params: { slug } } = useRoute()
+const { data } = await useAsyncData(
+  slug,
+  () =>  queryContent(`characters/${slug}`).findOne()
+) as any
 
 const characterData: Character = data
 
---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+const { data } = await useAsyncData(() =>  queryContent().find());
+
+const computedData = computed(() => {
+  return data?.value?.map((character) => {
+    character.link = `/characters/${character._path?.split("/").pop()}`;
+    return character;
+  })
+})
+
+</script>
+<template>
+  <div>
+    <NuxtLink
+      v-for="(character, index) in computedData"
+      :key="index"
+      :to="character.link"
+      >{{ character.name }}
+      </NuxtLink>
+  </div>
+</template>
