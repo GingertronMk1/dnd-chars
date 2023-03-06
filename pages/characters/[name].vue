f95270b559f3a8e8e4cd0fe9e0bbfa84b313e267
Jack Ellis <jackellis1504@gmail.com>
2023-03-06 11:56:01 +0000

    I have no idea what I'm doing

---
+++ pages/characters/[name].vue
@@ -0,0 +1,13 @@
+<script setup>
+const { params } = useRoute()
+// When accessing /posts/1, route.params.id will be 1
+const { data } = await useAsyncData(`${params.route}`, () => {
+  return queryContent().findOne();
+})
+
+
+</script>
+<template>
+  <pre v-text="data" />
+
+</template>
